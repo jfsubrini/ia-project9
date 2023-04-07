ca34b1d6dc38db40b5b41010e99c349e3b3de508
jfsubrini <jfsubrini@zoho.com>
2023-04-07 17:56:34 +0200

    Ready for production with Heroku

--- app_web_p9/app_web_p9/urls.py
+++ app_web_p9/app_web_p9/urls.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """app_web_p9 URL Configuration
-Deployed on Heroku at https://ia-project9.herokuapp.com/.  TODO
+Deployed on Heroku at https://ia-project9.herokuapp.com/.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -17,16 +17,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('web.urls')),
 ]
-
-# To see the images in debug mode.
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
